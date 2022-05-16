1d83d24f7e997064577b93d692ceba31e083ea81
orojlo <a.orojlo@gmail.com>
2022-05-16 20:21:17 +0430

    play with models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=90)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
