3bf5413a5ef5e45eebc8898dd02f649566035794
orojlo <a.orojlo@gmail.com>
2022-05-16 23:14:57 +0430

    play with url

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from post.views import homepage_view
+from post.views import about
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dashboard/', admin.site.urls),
+    path('', homepage_view),
+    path('about/', about)
 ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homepage_view(request):
+    return HttpResponse('<h1>Hello</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Nothing For now! </h1>')
