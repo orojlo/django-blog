f7488c2959ba0aebdd111a48b203cf9dd7aa18f0
orojlo <a.orojlo@gmail.com>
2022-05-16 23:05:39 +0430

    play with django admin filter

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,18 @@
+from pyexpat import model
 from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'published_date', 'content']
+    list_display_links = ['title', 'published_date']
+    list_filter = ['published_date']
+    search_fields = ['title', 'content']
+
+    class meta:
+        model = Post
+
+
+admin.site.register(Post, PostAdmin)
