1e62d2342afc7200ea1ad95653643552c7a9719e
orojlo <a.orojlo@gmail.com>
2022-05-16 22:51:55 +0430

    user verbose_name

--- post/models.py
+++ post/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=90)
-    content = models.TextField()
-    published_date = models.DateTimeField()
+    title = models.CharField(max_length=90, verbose_name="Post Title")
+    content = models.TextField(verbose_name="Post Content")
+    published_date = models.DateTimeField(verbose_name="Post Published Date")
 
     def __str__(self):
         return self.title
