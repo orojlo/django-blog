d3f3ab2de22a3490ebdc5ebf8ce93ac30978cf88
orojlo <a.orojlo@gmail.com>
2022-05-17 22:42:12 +0430

    play wit templates

--- aboutpage/views.py
+++ aboutpage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def about(request):
-    return HttpResponse('<h1>Nothing For now! </h1>')
+    return render(request, 'about.html', {})
--- homepage/views.py
+++ homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def homepage_view(request):
-    return HttpResponse('<h1>Hello From HomePage</h1>')
+    return render(request, 'home.html', {})
